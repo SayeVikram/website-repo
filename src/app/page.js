6322192493730c50861d05420336d22530cc21e5
Saye Vikram <87774705+SayeVikram@users.noreply.github.com>
2025-07-15 15:30:23 -0500

    combined files

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import HeroSection from "./HeroSection";
-import AboutMe from "./AboutMe";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] max-w-screen w-full right-0 left-0 absolute">
       <div className="sm:container px-12 py-4 mt-15 sm:mr-0 inset-0 bg-[#121212]">
         <HeroSection/>
-        <AboutMe/>
       </div>
     </main>
   );
